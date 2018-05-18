1f3fa70957a18a37734f5681eb5853c0f2f322ab
Bruno Tarmann <bruno@tarmann.com.br>
2018-05-18 16:05:43 +0100

    Fix typo

--- addon/components/radio-button.js
+++ addon/components/radio-button.js
@@ -45,7 +45,7 @@ export default Component.extend({
       this.sendAction('changed', newValue);
     },
     clicked(value) {
-      this.sendAction('changed', value);
+      this.sendAction('clicked', value);
     }
   }
 });
